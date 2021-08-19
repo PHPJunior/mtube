21ce98ba606c19a2cc7aca2414ac2a191eb99724
Iman Ghafoori <imanghafoori1@gmail.com>
2021-08-20 02:22:50 +0430

    Putting tus related stuff in TusServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,7 @@ namespace App\Providers;
 
 use App\Models\Channel\Video;
 use App\Observers\VideoObserver;
-use App\Services\GoogleDriveAdapter;
-use TusPhp\Tus\Server as TusServer;
-use App\Listeners\TusEventListener;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,19 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->singleton('tus-server', function ($app) {
-            Storage::disk('public')->makeDirectory('uploads');
-
-            $server = new TusServer('redis');
-            $server->setApiPath('/tus')->setUploadDir(storage_path('app/public/uploads'));
-
-            $listener = app(TusEventListener::class);
-            $server->event()->addListener('tus-server.upload.complete', [
-                $listener, 'handleUploadCompleted'
-            ]);
-
-            return $server;
-        });
+        //
     }
 
     /**
